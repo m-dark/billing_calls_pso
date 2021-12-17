61db5cbb578cc6619ff9fe2f1b25fc24b2ae0bec
TEL_OEP <kruk.ivan@itmh.ru>
2021-12-17 13:20:08 +0500

    send email

--- main.py
+++ main.py
@@ -5,7 +5,17 @@ import pymysql
 import sys
 import re
 import logging
+import configemail
 from datetime import datetime
+import smtplib
+# Добавляем необходимые подклассы - MIME-типы
+import mimetypes                                            # Импорт класса для обработки неизвестных MIME-типов, базирующихся на расширении файла
+from email import encoders                                  # Импортируем энкодер
+from email.mime.base import MIMEBase                        # Общий тип
+from email.mime.text import MIMEText                        # Текст/HTML
+from email.mime.image import MIMEImage                      # Изображения
+from email.mime.audio import MIMEAudio                      # Аудио
+from email.mime.multipart import MIMEMultipart              # Многокомпонентный объект
 
 date_time = datetime.strftime(datetime.now(), "%Y.%m.%d_%H-%M-%S")
 dir_log = '/opt/asterisk/billing_calls_pso/log/'
@@ -20,6 +30,64 @@ log.info('Start "'+sys.argv[1]+' '+sys.argv[2]+' '+sys.argv[3]+' '+sys.argv[4]+'
 array_argv = []
 outcid_argv = set([])
 outcid_db = set([])
+
+def send_email(addr_to, msg_subj, msg_text, files):
+    addr_from = configemail.hostname.nodename               # Отправитель
+##    password  = "password"                                  # Пароль
+    msg = MIMEMultipart()                                   # Создаем сообщение
+    msg['From']    = addr_from                              # Адресат
+    msg['To']      = addr_to                                # Получатель
+    msg['Subject'] = msg_subj                               # Тема сообщения
+    body = msg_text                                         # Текст сообщения
+    msg.attach(MIMEText(body, 'plain'))                     # Добавляем в сообщение текст
+    process_attachement(msg, files)
+
+    #======== Этот блок настраивается для каждого почтового провайдера отдельно ===============================================
+##    server = smtplib.SMTP_SSL('127.0.0.1', 465)           # Создаем объект SMTP
+    #server.starttls()                                      # Начинаем шифрованный обмен по TLS
+    #server.set_debuglevel(True)                            # Включаем режим отладки, если не нужен - можно закомментировать
+##    server.login(addr_from, password)                     # Получаем доступ
+    server = smtplib.SMTP('localhost')
+    server.set_debuglevel(0)
+    server.send_message(msg)                                # Отправляем сообщение
+    server.quit()                                           # Выходим
+    #==========================================================================================================================
+
+def process_attachement(msg, files):                        # Функция по обработке списка, добавляемых к сообщению файлов
+    for f in files:
+        if os.path.isfile(f):                               # Если файл существует
+            attach_file(msg,f)                              # Добавляем файл к сообщению
+        elif os.path.exists(f):                             # Если путь не файл и существует, значит - папка
+            dir = os.listdir(f)                             # Получаем список файлов в папке
+            for file in dir:                                # Перебираем все файлы и...
+                attach_file(msg,f+"/"+file)                 # ...добавляем каждый файл к сообщению
+
+def attach_file(msg, filepath):                             # Функция по добавлению конкретного файла к сообщению
+    filename = os.path.basename(filepath)                   # Получаем только имя файла
+    ctype, encoding = mimetypes.guess_type(filepath)        # Определяем тип файла на основе его расширения
+    if ctype is None or encoding is not None:               # Если тип файла не определяется
+        ctype = 'application/octet-stream'                  # Будем использовать общий тип
+    maintype, subtype = ctype.split('/', 1)                 # Получаем тип и подтип
+    if maintype == 'text':                                  # Если текстовый файл
+        with open(filepath) as fp:                          # Открываем файл для чтения
+            file = MIMEText(fp.read(), _subtype=subtype)    # Используем тип MIMEText
+            fp.close()                                      # После использования файл обязательно нужно закрыть
+    elif maintype == 'image':                               # Если изображение
+        with open(filepath, 'rb') as fp:
+            file = MIMEImage(fp.read(), _subtype=subtype)
+            fp.close()
+    elif maintype == 'audio':                               # Если аудио
+        with open(filepath, 'rb') as fp:
+            file = MIMEAudio(fp.read(), _subtype=subtype)
+            fp.close()
+    else:                                                   # Неизвестный тип файла
+        with open(filepath, 'rb') as fp:
+            file = MIMEBase(maintype, subtype)              # Используем общий MIME-тип
+            file.set_payload(fp.read())                     # Добавляем содержимое общего типа (полезную нагрузку)
+            fp.close()
+            encoders.encode_base64(file)                    # Содержимое должно кодироваться как Base64
+    file.add_header('Content-Disposition', 'attachment', filename=filename) # Добавляем заголовки
+    msg.attach(file)                                        # Присоединяем файл к сообщению
 
 def check_number(number):
     result_number=re.match(r'((all)|([3][4][3][23]\d{6})|([78][3][4][3][23]\d{6})|([23]\d{6}))$', number)
@@ -121,4 +189,15 @@ for cid in outcid_argv:
 file_calls.close()
 cursor.close()
 asteriskcdrdb.close()
+
+# Использование функции send_email()
+addr_to = configemail.email_to                                      # Получатель
+files = [dir_log+date_time+'.csv']                                  # Список файлов, если вложений нет, то files=[]
+
+numbers = array_argv[5]
+if array_argv[5] == 'all':
+    numbers = outcid_db
+
+send_email(addr_to, 'Отчет по звонкам', 'Информация о звонках'+"\n"+'С:  '+str(array_argv[1])+' '+str(array_argv[2])+"\n"+'До: '+str(array_argv[3])+' '+str(array_argv[4])+''+"\n"+'По номерам: '+str(outcid_db)+"\n", files)
+
 log.info('End "'+date_time+'.csv"')
